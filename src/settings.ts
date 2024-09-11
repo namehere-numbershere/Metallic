33a15e707d7bce5beafa8d76096bc18b05de12b6
NamehereNumbersHere <111458916+namehere-numbershere@users.noreply.github.com>
2024-09-11 18:43:37 -0400

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,7 @@
 const obfuscate = true;
 const rammerhead = true;
 const github = "https://github.com/cognetwork-dev/Metallic";
-const discord = "https://discord.com/invite/yk33HZSZkU";
+const discord = "";
 const privacyPolicyUpdated = 1707797140659;
 const gamesCdn = "https://radon.games";
 
