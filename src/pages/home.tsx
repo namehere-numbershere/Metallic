b22faccba402539fbd7a30059b0888d1f1f72237
NamehereNumbersHere <111458916+namehere-numbershere@users.noreply.github.com>
2024-12-12 19:24:21 -0500

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -188,11 +188,13 @@ function Home() {
 					</span>
 				</h1>
 			) : (
+			<div>
 				<h1 class="title text-4xl sm:text-6xl font-bold text-center mt-32 mb-8 sm:mb-16 font-title">
 					<Obfuscated>NHMath</Obfuscated>
 				</h1>
 				<p> e </p>
-			)}
+			</div>
+				)}
 			<div class="flex flex-col items-center justify-center">
 				<div
 					class={
